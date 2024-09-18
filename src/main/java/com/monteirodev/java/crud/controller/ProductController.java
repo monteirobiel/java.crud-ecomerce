6f1902f70a3d02f9f327825d927e761164b56425
gabrielmonteiro <gabriel.m.oliveira03@gmail.com>
2024-09-18 13:42:05 -0300

    feat: add new controller with get mapping request.

--- src/main/java/com/monteirodev/java/crud/controller/ProductController.java
+++ src/main/java/com/monteirodev/java/crud/controller/ProductController.java
@@ -1,4 +1,20 @@
 package com.monteirodev.java.crud.controller;
 
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/product")
 public class ProductController {
+
+    @GetMapping
+    public ResponseEntity getAllProducts() {
+        return ResponseEntity.ok("Its ok!");
+    }
+
 }
