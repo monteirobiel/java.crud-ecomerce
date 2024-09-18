bb57e7779d13aa11c6182d7bfaf66a2b8f8962bc
gabrielmonteiro <gabriel.m.oliveira03@gmail.com>
2024-09-18 14:25:27 -0300

    feat: add post method, Autowired annotation and changed return from get mapping method.

--- src/main/java/com/monteirodev/java/crud/controller/ProductController.java
+++ src/main/java/com/monteirodev/java/crud/controller/ProductController.java
@@ -1,20 +1,28 @@
 package com.monteirodev.java.crud.controller;
 
-import org.apache.coyote.Response;
+import com.monteirodev.java.crud.domain.Product;
+import com.monteirodev.java.crud.domain.ProductRepository;
+import com.monteirodev.java.crud.domain.RequestProduct;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/product")
 public class ProductController {
 
+    @Autowired
+    private ProductRepository repository;
+
+
     @GetMapping
     public ResponseEntity getAllProducts() {
-        return ResponseEntity.ok("Its ok!");
+        return ResponseEntity.ok(repository.findAll());
     }
 
+    @PostMapping
+    public ResponseEntity registerProduct(@RequestBody @Valid RequestProduct data){
+        return ResponseEntity.ok(repository.save(new Product(data)));
+    }
 }
