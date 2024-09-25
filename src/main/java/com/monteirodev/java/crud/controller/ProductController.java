38dc2ed948d7018eb337899dff89b992d4776b57
gabrielmonteiro <gabriel.m.oliveira03@gmail.com>
2024-09-25 12:24:24 -0300

    feat(ProductController): add new methods for update and delete

--- src/main/java/com/monteirodev/java/crud/controller/ProductController.java
+++ src/main/java/com/monteirodev/java/crud/controller/ProductController.java
@@ -3,10 +3,14 @@ package com.monteirodev.java.crud.controller;
 import com.monteirodev.java.crud.domain.Product;
 import com.monteirodev.java.crud.domain.ProductRepository;
 import com.monteirodev.java.crud.domain.RequestProduct;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/product")
@@ -22,7 +26,27 @@ public class ProductController {
     }
 
     @PostMapping
-    public ResponseEntity registerProduct(@RequestBody @Valid RequestProduct data){
+    public ResponseEntity registerProduct(@RequestBody @Valid RequestProduct data) {
         return ResponseEntity.ok(repository.save(new Product(data)));
+    }
+
+    @PutMapping
+    public ResponseEntity updateProduct(@RequestBody @Valid RequestProduct data) {
+        Optional<Product> optionalProduct = repository.findById(data.id());
+        if (optionalProduct.isPresent()) {
+            Product product = optionalProduct.get();
+            product.setName(data.name());
+            product.setPrice_in_cents(data.price_in_cents());
+            repository.save(product);
+            return ResponseEntity.ok(product);
+        } else {
+            throw new EntityNotFoundException();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteProduct(@PathVariable long id) {
+        repository.deleteById(id);
+        return ResponseEntity.ok().build();
     }
 }
