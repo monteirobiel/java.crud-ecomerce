9bb9847fc1320a407c5db5db321fc2425f2c1fb4
gabrielmonteiro <gabriel.m.oliveira03@gmail.com>
2024-09-25 12:23:32 -0300

    feat(RequestProduct): add validations.

--- src/main/java/com/monteirodev/java/crud/domain/RequestProduct.java
+++ src/main/java/com/monteirodev/java/crud/domain/RequestProduct.java
@@ -1,4 +1,13 @@
 package com.monteirodev.java.crud.domain;
 
-public record RequestProduct(Long id, String name, Integer price_in_cents) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record RequestProduct(
+
+        Long id,
+        @NotBlank(message = "Name is mandatory")
+        String name,
+        @NotNull(message = "Price is mandatory")
+        Integer price_in_cents) {
 }
